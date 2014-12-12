8eb4e05832f6c90a5cd5b8707b2ff2664b183c56
JonHid <jon_hid@hotmail.com>
2014-12-12 10:46:45 +0100

    Merge ivan into jonaMerge
    Conflicts:
    	index.html

--- js/articulos.js
+++ js/articulos.js
@@ -1,18 +1,35 @@
+function pintarArticulos($articulo){
+    datos="<div class='col-xs-12 col-sm-6 col-lg-4'>\n\
+                <article id='producto01' class='articulo  thumbnail'>\n\
+                    <img class='producto_img' src='style/img/articulos/"+$articulo.imagenArticulo+".png' alt='"+$articulo.nombreArticulo+"'/>\n\
+                    <div class='caption'>\n\
+                        <p class='producto_titulo'>"+$articulo.nombreArticulo+"</p>\n\
+                        <p class='producto_descripcion'>"+$articulo.descripcionArticulo+"</p>\n\
+                        <p class='producto_precio col-xs-6'>"+$articulo.precioArticulo+"€</p>\n\
+                        <p class='producto_boton col-xs-6'>\n\
+                            <a href='#' class='btn btn-primary' role='button'>\n\
+                                <span class='glyphicon glyphicon-shopping-cart'></span>\n\
+                            </a>\n\
+                        </p>\n\
+                    </div>\n\
+                </article>\n\
+            </div>";
+    
+    return datos;
+}
+
 function mostrarArticulos($tipo) {
 
     $promesa = getAjax("articulo", "asc");
 
     $promesa.success(function (data) {
 
-        var datos = "";
+        datos = "";
 
         $.each(data, function (index) {
 
             if ($tipo === data[index].tipoArticulo) {
-
-                datos += "<div class='articulos'>\n\
-                <img src='style/img/articulos/" + data[index].imagenArticulo + ".png' alt='" + data[index].nombreArticulo + "'>\n\
-                </div>";
+                datos += pintarArticulos(data[index]);
             }
         });
         $("#articulos").html(datos);
--- js/mostrarPorPlataformas.js
+++ js/mostrarPorPlataformas.js
@@ -1,12 +1,12 @@
-function mostrarPorPlataformas($plataforma){
-    $promesa=getAjax("articulo","asc");
-    
-    $promesa.success(function (data){
-        datos="";
-        $.each(data,function(index){
-            
-            if(data[index].plataformaArticulo===$plataforma){
-             datos+="<div class='articulo'><img src='style/img/articulos/"+data[index].imagenArticulo+".png' alt="+data[index].nombreArticulo+"> </div>";
+function mostrarPorPlataformas($plataforma) {
+    $promesa = getAjax("articulo", "asc");
+
+    $promesa.success(function (data) {
+        datos = "";
+        $.each(data, function (index) {
+
+            if (data[index].plataformaArticulo === $plataforma) {
+                datos += pintarArticulos(data[index]);
             }
         });
         $("#articulos").html(datos);
