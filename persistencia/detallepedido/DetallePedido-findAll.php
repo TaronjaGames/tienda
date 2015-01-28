f3b5aec613030a6334ee12635536a68793dea201
positronio72 <positronio72@gmail.com>
2015-01-28 13:34:32 +0100

    tienda_sprint_03

--- persistencia/detallepedido/DetallePedido-findAll.php
+++ persistencia/detallepedido/DetallePedido-findAll.php
@@ -49,6 +49,13 @@ switch ($examp) {
                 . " concat(round(cantidadArticulo*precioArticulo,2),' €') AS importeLinea"
                 . " FROM detallepedido WHERE idPedido=" . $id . " ORDER BY $sidx $sord LIMIT $start , $limit";
         
+//        $SQL = "SELECT d.idDetallePedido, a.nombreArticulo, d.cantidadArticulo,"
+//                . " concat(d.precioArticulo, ' €') AS precioArticulo, d.idPedido,"
+//                . " concat(round(d.cantidadArticulo*d.precioArticulo,2),' €') AS importeLinea"
+//                . " FROM detallepedido d INNER JOIN articulo a"
+//                . " ON d.idArticulo = a.idArticulo"
+//                . " WHERE d.idPedido=" . $id . " ORDER BY $sidx $sord LIMIT $start , $limit";
+        
         $result = $conexion->query($SQL);
 
         $respuesta = new stdClass();
