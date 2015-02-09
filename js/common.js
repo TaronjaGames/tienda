7d14e2662d3b9d31fe3cff41499c741ff24bfaeb
Fran <fmnf72@gmail.com>
2015-02-09 22:22:23 +0100

    fran_banco_validaciones_cliente

--- js/common.js
+++ js/common.js
@@ -5,7 +5,7 @@ usuarioLogueado = "";//Contiene el usuario logueado
 accionPrevia = "";//Contiene la id del elemento que desencadena el último evento
 //valor: this.id
 
-codigoCuentaBancaria = "E002-S002-1";
+codigoCuentaBancaria = "0002-0002-0001";
 
 
 
