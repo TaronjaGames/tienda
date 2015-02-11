c8c2c3c48d3299bad3bc6e8dbcadd9477e2b8eef
JonHid <jon_hid@hotmail.com>
2015-02-11 13:10:40 +0100

    Merge origin/develop_new_merge into develop_aux

--- persistencia/PDF/GenerarPDF.js
+++ persistencia/PDF/GenerarPDF.js
@@ -1,12 +1,15 @@
-function generarPDF(miPDF) {
-    var jsPDF = require('libs/jsPDF').jsPDF; 
+function abrirPestana() {
+    window.open('persistencia/PDF/newPDF.html');
+}
+
+function generarPDF() {
     var doc = new jsPDF();
+
     doc.setFontSize(22);
-    doc.text(20, 20, 'Titulo');
+    doc.text(20, 20, 'This is a title');
+
     doc.setFontSize(16);
-    doc.text(20, 30, 'PDF GENERADO');
-    
-     var file = Ti.Filesystem.getFile(Ti.Filesystem.tempDirectory, miPDF);
-     file.write(doc.output());
-     return file;
+    doc.text(20, 30, 'This is some normal sized text underneath.');
+
+    doc.output('datauri');
 }
